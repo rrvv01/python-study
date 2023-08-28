821dbe3a5b9919c1135e6465f5798d4501f45662
김근수 <guksuukim@naver.com>
2023-08-29 06:53:35 +0900

    제발

---
+++ gg.py
@@ -0,0 +1,37 @@
+'''
+브랜드 = ['k5', 'BMW']
+
+if 'k6' in 브랜드 :
+    print('주문가능합니다')
+else :
+    print('주문 안됨 ㅅㄱ')
+    '''
+
+
+'''
+파일 = open('a.txt', 'w')
+파일.write('hello')
+파일.close()
+
+파일 = open('a.txt', 'a')
+파일.write('\n반가워')
+파일.close()
+
+
+파일 = open('a.txt', 'r')
+print(파일.read() )
+파일.close()
+'''
+
+#f = open('data.csv', 'w')
+#f.write('김,이,박')
+#f.close()
+
+리스트 = open('list.txt', 'w')
+리스트.write('\n삼성전자')
+리스트.write('\n카카오')
+리스트.write('\n네이버')
+리스트.write('\n신풍제약')
+리스트.close()
+
+
